b7fb61a3a1a4af1eb26a8b688e36eaa88a2a4171
Virginia García <vickkygm1@gmail.com>
2018-02-23 10:58:07 -0600

    cookies

---
+++ lovefunction.py
@@ -0,0 +1,16 @@
+
+#' A cookie function
+#' 
+#' This function allows you to express your love for cookies
+#' @param love - Recieve TRUE or FALSE
+#' @keywords cats
+#' @export
+#' @examples 
+#' cat_function()
+
+def cookies_function(love):
+  if love == TRUE :
+    print("I love cookies!!")
+  else:
+    print("I'm not a cool person :( !!")
+  return
