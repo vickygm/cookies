8de50816959ae8df90d4e5b81f0810e501cda930
Fer Flores <foal.90@gmail.com>
2018-02-26 17:16:19 -0600

    intento 7

--- cookies/__init__.py
+++ cookies/__init__.py
@@ -1,7 +1,7 @@
 
 
 def cookies_function(love):
-  if love == True:
+  if love == False:
    return print("I love cookies!!")
   else:
    return print("I'm not a cool person :( !!")
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cookies',
-      version='0.4',
+      version='0.5',
       description='Cool person or not',
       url='https://github.com/vickygm/cookies',
       author='Moana',
